b24dedd962709ab8cc34e5bdf6677aa6c03ffca4
Kamil Makowski <kamil.makowski2@tietoevry.com>
2026-03-13 11:23:45 +0100

    fix: change dockerfile v2

--- new-image-build-v2.sh
+++ new-image-build-v2.sh
@@ -1,6 +1,6 @@
 az acr build `
     --registry "bridgeprodgithubrunnersacr" `
-    --image "github-actions-runner-v2:0.1" `
+    --image "github-actions-runner-v2:1.0" `
     --file "Dockerfilev2.github" `
     --auth-mode Default `
     "https://github.com/Banking-Bridge/sh-runner-image.git"
